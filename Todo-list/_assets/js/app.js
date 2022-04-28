b3981c152d9149dc5eaa1b17ce9ae5c71cd2b2a4
Pedro Henrique <phc.contato22@gmail.com>
2022-04-28 18:33:14 -0300

    Criando elemento dinâmico

--- Todo-list/_assets/js/app.js
+++ Todo-list/_assets/js/app.js
@@ -4,7 +4,7 @@ let btnEdit = document.querySelectorAll(".btn-edit");
 let btnExcluir = document.querySelectorAll(".btn-exit");
 let tarefas = document.querySelectorAll(".tarefa");
 
-btnCadastro.addEventListener("click", () =>{
+btnCadastro.addEventListener("click", () =>{  
     let cadastroValue = inputCadastro.value;
     
     if(cadastroValue === ""){
@@ -12,7 +12,7 @@ btnCadastro.addEventListener("click", () =>{
 
     } else{
        alert('Cadastrado com sucesso');
-
+       criandoTarefa(cadastroValue); 
        let tarefa = new Tarefa(cadastroValue);
        guardandoTarefa(tarefa);
        inputCadastro.value = "";
@@ -49,6 +49,65 @@ function gravar(tarefa) {
         localStorage.setItem('id', id)
     }
 }
+
+// Criando elemento no HTML
+const container = document.querySelector(".container-main");
+function criandoTarefa(text){
+
+    //Criando a div da tarefa
+    let divTarefa = document.createElement('div');
+    divTarefa.className = "tarefa";
+
+    //Criando a descrição
+    let divDescricao = document.createElement("div");
+    divDescricao.classList.add("descricao");
+
+    let inputCheck = document.createElement('input')
+    inputCheck.type = "checkbox";
+
+    let textoTarefa = document.createElement('p');
+    let texto = document.createTextNode(text);
+    textoTarefa.appendChild(texto);
+
+    divDescricao.appendChild(inputCheck);
+    divDescricao.appendChild(textoTarefa);
+
+
+
+    //Criando a os botoes
+    let divBotoes = document.createElement("div");
+    divBotoes.classList.add('btns')
+
+    //Botão editar
+    let btnEditar = document.createElement('button');
+    btnEditar.type = "submit";
+    btnEditar.classList.add('btn-edit');
+
+    btnEditar.innerHTML = '<i class="fa-solid fa-pen-to-square"></i>';
+
+    divBotoes.appendChild(btnEditar);
+
+    //Botao excluir
+    let btnExcluir = document.createElement('button');
+    btnExcluir.type = "submit";
+    btnExcluir.classList.add('btn-exit');
+
+    btnExcluir.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+
+    divBotoes.appendChild(btnExcluir);
+
+
+
+    //Inserindo no HTML
+    divTarefa.appendChild(divDescricao)
+    divTarefa.appendChild(divBotoes)
+    container.appendChild(divTarefa)
+   
+
+}
+
+
+
 
 
 
