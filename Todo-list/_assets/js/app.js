a6a6f1bda97cf500d6b0eab76f0980e717def4b0
Pedro Henrique <phc.contato22@gmail.com>
2022-04-30 23:36:35 -0300

    Todo list

--- Todo-list/_assets/js/app.js
+++ Todo-list/_assets/js/app.js
@@ -9,7 +9,7 @@ btnCadastro.addEventListener("click", () =>{
        alert("Valor invalido, verifique novamente");
 
     } else{
-       alert('Cadastrado com sucesso');
+      // alert('Cadastrado com sucesso');
        let tarefa = new Tarefa(cadastroValue);
        guardandoTarefa(tarefa);
        inputCadastro.value = "";
@@ -26,17 +26,20 @@ class Tarefa {
 function guardandoTarefa(tarefa){
    //gravar(tarefa)
     gravar(tarefa);
-    recuperarTarefa();
+
+    //Enviando a tarefa para a criação no HTML
+    let listaTarefas = recuperarTarefa();
+    conteudoTarefa(listaTarefas);
     
 }
 
-
+//Gerando o ID da nova tarefa
 function proximoId(){
     let proximoId = localStorage.getItem('id')
     return parseInt(proximoId) + 1;
 }
 
-
+//Armazenando a tarefa cadastrada no LocalStorage
 function gravar(tarefa) {
     if(localStorage.getItem('id') != null){
         let id = proximoId();
@@ -58,49 +61,51 @@ function recuperarTarefa(){
     for(let i = 0; i <= id; i++){
         let tarefaId = JSON.parse(localStorage.getItem(i));
 
-        if(tarefaId === null){
+       if(tarefaId === null){
             continue
-        }
+       }
+
 
         tarefaId.id = i;
         tarefas.push(tarefaId)
-    }
 
-    let idPosition = localStorage.getItem('id');
-    
-    let teste = tarefas[idPosition].tarefa;
-    criandoTarefa(teste); 
-   // criandoTarefa(teste)
-  
+        
+    }
+   
+   
     return tarefas;
     
 }
 
+//Enviando o conteudo da tarefa para o HTML dinâmico
+function conteudoTarefa(listaTarefas){
+    let novaTarefaID = localStorage.getItem('id');
+    let conteudoNovaTarefa = listaTarefas[novaTarefaID].tarefa;
+    //window.location.reload();
 
-
-
+    return criandoTarefa(conteudoNovaTarefa);
+}
 
 
 // Criando elemento no HTML
 const container = document.querySelector(".container-main");
+
 function criandoTarefa(text){
 
+  
     //Criando a div da tarefa
     let divTarefa = document.createElement('div');
     divTarefa.className = "tarefa";
+    
 
     //Criando a descrição
     let divDescricao = document.createElement("div");
     divDescricao.classList.add("descricao");
 
-    let inputCheck = document.createElement('input')
-    inputCheck.type = "checkbox";
-
     let textoTarefa = document.createElement('p');
     let texto = document.createTextNode(text);
     textoTarefa.appendChild(texto);
 
-    divDescricao.appendChild(inputCheck);
     divDescricao.appendChild(textoTarefa);
 
 
@@ -109,7 +114,7 @@ function criandoTarefa(text){
     let divBotoes = document.createElement("div");
     divBotoes.classList.add('btns')
 
-    //Botão editar
+    //Botão confirmar
     let btnEditar = document.createElement('button');
     btnEditar.type = "submit";
     btnEditar.classList.add('btn-edit');
@@ -130,15 +135,162 @@ function criandoTarefa(text){
 
 
     //Inserindo no HTML
-    divTarefa.appendChild(divDescricao)
-    divTarefa.appendChild(divBotoes)
-    container.appendChild(divTarefa)
+    divTarefa.appendChild(divDescricao);
+    divTarefa.appendChild(divBotoes);
+    container.appendChild(divTarefa);
+
+
+    
+
+   
+ 
+}
+
+
+//Criando as tarefas já existentes ao acessar a página
+window.addEventListener("load", () =>{
+    let tarefasExistentes = recuperarTarefa();
+    
+    
+    for(let i = 0; i  <= (tarefasExistentes.length - 1); i++) {
+            let conteudoTarefa = tarefasExistentes[i].tarefa;
+             criandoTarefa(conteudoTarefa)
+              
+    }
+
+  
+
+    let divs = document.querySelectorAll('.tarefa')
+    //console.log(divs[0]);
+
+    let btnEditar = document.querySelectorAll('.btn-edit')
+    //console.log(btnEditar);
+
+    let btnExcluir = document.querySelectorAll('.btn-exit')
+    //console.log(btnExcluir);
+
+    funcionalidadesBotoes(divs, btnEditar, btnExcluir);  
    
 
+    
+
+})
+
+let globalTarefas = recuperarTarefa();
+
+function removendoItem(id){
+    localStorage.removeItem(id)
+    let idLocal = localStorage.getItem('id') - 1;
+    let newCapacity = localStorage.setItem('id', idLocal)
+
+    /*
+    for(let i = 0; i <= globalTarefas.length; i++){
+        if(i <= idLocal){
+            globalTarefas[i] = localStorage.setItem('id', i)
+        }
+
+        for(let n = globalTarefas.length; n > idLocal; n--){
+            localStorage.removeItem(n)
+        }
+    } */
+
+   
+
+    return newCapacity
+   
+    
+    
 }
 
 
 
+//Funcionalidades botes
+function funcionalidadesBotoes(div, btnEdit, btnExluir){
+  /*
+    btnExluir.forEach(element => {
+        element.addEventListener("click", () =>{
+            switch(element){
+                case btnExluir[0]:
+                        div[0].remove();
+                        removendoItem(0);
+                    break;
+
+                case btnExluir[1]:
+                    div[1].remove();
+                        removendoItem(1);
+                    break;
+
+                case btnExluir[2]:
+                    div[2].remove();
+                        removendoItem(2);
+                    break;
+
+                case btnExluir[3]:
+                    div[3].remove();
+                    removendoItem(3);
+                    break;
+
+                
+            }
+        })
+    });
+*/
+
+  
+
+
+
+  /* btnExluir.addEventListener("click", () =>{
+        div.remove();
+        removendoItem(0)
+       
+   }) */
+
+  /*
+   btnEdit.addEventListener("click", (item) =>{
+        let listaTarefas = recuperarTarefa();
+
+        console.log(listaTarefas);
+       
+
+        for(let i = 0; i<= listaTarefas.length; i++){
+            let mostrandoTodosValores = listaTarefas[i].tarefa;
+            if(mostrandoTodosValores[i] == item[i]){
+               
+            }
+        }
+        
+        
+        let alterandoTarefaID = localStorage.getItem('id');
+        let conteudoNovaTarefa = listaTarefas[alterandoTarefaID].tarefa;
+
+        console.log(conteudoNovaTarefa); 
+        
+   }) 
+    */
+    
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
 
 
 
